f634c0ca551c7a3c0f789f7b5d13542589a6b309
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-29 19:19:08 +0300

    new commit

--- project001/class002.py
+++ project001/class002.py
@@ -8,6 +8,23 @@ with open('file/employees.json') as f:
     for item in data:
         print(item)
 
+    # Print the number of items in the JSON data
+    print(len(data))
+
+    # Print the first item in the JSON data
+    print(data[0])
+
+    # Print the last item in the JSON data
+    print(data[-1])
+
+    # Print the third item in the JSON data
+    print(data[2])
+
+    # Print the last three items in the JSON data
+    print(data[-3:])
+
+    # Print the first three items in the JSON data
+    print(data[:3])
     # Close the file
     f.close()
 
